359aa214d4829c157cb536a1974f57a85c1c82a1
TarushShankar <tarush@ualberta.ca>
2022-09-28 19:41:59 -0600

    Update Shape.java

--- app/src/main/java/com/mgaid/cmput301f22t26/Shape.java
+++ app/src/main/java/com/mgaid/cmput301f22t26/Shape.java
@@ -3,7 +3,6 @@ package com.mgaid.cmput301f22t26;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "blue";
     Shape(int x, int y){
         this.x = x;
         this.y = y;
