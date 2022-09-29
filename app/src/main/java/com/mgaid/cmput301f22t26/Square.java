5609664c6853ff525ab38e4efb0711f9f60211d5
TarushShankar <tarush@ualberta.ca>
2022-09-28 19:47:18 -0600

    Changed the Square class to WeirdShape class

--- app/src/main/java/com/mgaid/cmput301f22t26/Square.java
+++
@@ -1,7 +0,0 @@
-package com.mgaid.cmput301f22t26;
-
-public class Square extends Shape{
-    public Square(int x, int y){
-        super(x,y);
-    }
-}
---
+++ app/src/main/java/com/mgaid/cmput301f22t26/WeirdShape.java
@@ -0,0 +1,8 @@
+package com.mgaid.cmput301f22t26;
+
+public class WeirdShape extends Shape{
+    WeirdShape(int x,int y){
+        super(x,y);
+        String str = "Conflict Resolved";
+    }
+}
