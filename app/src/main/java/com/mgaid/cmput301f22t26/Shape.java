1a9943979bc9fd647acbdb407855f1419cc2916f
gaidukev <gaidukev@ualberta.ca>
2022-09-28 20:13:10 -0600

    change color

--- app/src/main/java/com/mgaid/cmput301f22t26/Shape.java
+++ app/src/main/java/com/mgaid/cmput301f22t26/Shape.java
@@ -3,7 +3,7 @@ package com.mgaid.cmput301f22t26;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "wenge";
+    private String color = "red";
     Shape(int x, int y){
         this.x = x;
         this.y = y;
