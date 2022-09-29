6d26d578cef07aaad80da5fa62e19f5c4c23be9c
gaidukev <gaidukev@ualberta.ca>
2022-09-28 19:57:42 -0600

    added color attribute

--- app/src/main/java/com/mgaid/cmput301f22t26/Shape.java
+++ app/src/main/java/com/mgaid/cmput301f22t26/Shape.java
@@ -3,6 +3,7 @@ package com.mgaid.cmput301f22t26;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "MAROON";
 
     Shape(int x, int y){
         this.x = x;
--- app/src/main/java/com/mgaid/cmput301f22t26/Square.java
+++ app/src/main/java/com/mgaid/cmput301f22t26/Square.java
@@ -2,6 +2,6 @@ package com.mgaid.cmput301f22t26;
 
 public class Square extends Shape{
     Square(){
-        
+
     }
 }
