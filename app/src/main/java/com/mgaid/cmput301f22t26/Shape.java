fbcbefeb2da85f27e280d039b4b95c3b4f2d2919
gaidukev <gaidukev@ualberta.ca>
2022-09-28 19:14:39 -0600

    Added app

---
+++ app/src/main/java/com/mgaid/cmput301f22t26/Shape.java
@@ -0,0 +1,16 @@
+package com.mgaid.cmput301f22t26;
+
+public abstract class Shape {
+    private int x;
+    private int y;
+
+    Shape(int x, int y){
+        this.x = x;
+        this.y = y;
+
+    }
+
+    Shape(){
+        
+    }
+}
